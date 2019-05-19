2ef9525a7856009d114bafd0e782733c2ee50949
zainosaurus <zain.tahlilkar@gmail.com>
2019-05-19 14:44:16 -0400

    Added setup script (adds source to ~/.bash_profile)

--- setup.sh
+++ setup.sh
@@ -1,30 +1,8 @@
-# General Aliases
-alias l='ls -GFh'
-alias ls='ls -GFh'
-alias la='ls -A'
-alias ll='ls -l'
-alias lla='ls -la'
+#!/bin/bash
 
-# Git Aliases
-LOG="git log --graph --decorate --format=format:'%C(yellow)%h%C(reset) - %C(bold red)%d%C(reset) %s %C(bold blue)(%ar)%C(reset)'"
-alias gs='git status'
-alias gl="$LOG -n 7"
-alias gll="$LOG"
-alias gdiff='git diff'
-alias gc='git checkout'
-alias gb='git branch'
-alias gm='git commit -m'
-alias gmerge='git merge'
-alias pull='git pull'
-alias push='git push'
+# Adds aliases, prompt config to ~/.bash_profile
+echo "source $(pwd)/aliases" >> ~/.bash_profile
+echo "source $(pwd)/prompt" >> ~/.bash_profile
 
-# Docker Aliases
-alias d='docker'
-alias dc='docker-compose'
-alias dcp='docker cp'
-alias dex='docker exec -it'
-alias dls='docker image ls'
-alias dps='docker ps'
-alias da='docker attach'
-alias dprune='docker system prune'
-
+# Adds .vimrc
+#echo "$(pwd)/.vimrc" >> ~/.vimrc

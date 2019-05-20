ccb97fab050f79fe90fc262f76bdc009f4007722
zainosaurus <zain.tahlilkar@gmail.com>
2019-05-20 14:55:33 -0400

    Completed prompt config - added git branch info

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+echo >> ~/.bash_profile
+echo "#Following configurations added by Zain Tahlilkar's config script:" >> ~/.bash_profile
+
 # Adds aliases, prompt config to ~/.bash_profile
 echo "source $(pwd)/aliases" >> ~/.bash_profile
 echo "source $(pwd)/prompt" >> ~/.bash_profile
 
+# External scripts (add to path)
+echo 'export PATH=$PATH:$(pwd)/external_scripts/' >> ~/.bash_profile
+
+# diff-so-fancy: for easier-to-read git diffs
+git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
+
+
 # Adds .vimrc
-#echo "$(pwd)/.vimrc" >> ~/.vimrc
+cat "$(pwd)/.vimrc" > ~/.vimrc

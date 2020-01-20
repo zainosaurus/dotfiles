797897a003603d988b64ef0b3e00700fb9470c0e
zainosaurus <zain.tahlilkar@gmail.com>
2020-01-20 14:28:57 -0500

    Added git autocompletion & updated aliases

--- setup.sh
+++ setup.sh
@@ -3,16 +3,22 @@
 echo >> ~/.bash_profile
 echo "#Following configurations added by Zain Tahlilkar's config script:" >> ~/.bash_profile
 
-# Adds aliases, prompt config to ~/.bash_profile
-echo "source $(pwd)/aliases" >> ~/.bash_profile
-echo "source $(pwd)/prompt" >> ~/.bash_profile
+# Install Bash Completion - requires homebrew
+#brew install bash_completion
 
 # External scripts (add to path)
 echo "export PATH=\$PATH:$(pwd)/external_scripts/" >> ~/.bash_profile
+
+# Adds aliases, prompt config to ~/.bash_profile
+echo "source $(pwd)/external_scripts/git-completion.bash" >> ~/.bash_profile
+echo "source $(pwd)/aliases" >> ~/.bash_profile
+echo "source $(pwd)/prompt" >> ~/.bash_profile
+echo "#--- end of Zain's script ---" >> ~/.bash_profile
+echo >> ~/.bash_profile
 
 # diff-so-fancy: for easier-to-read git diffs
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 
 
-# Adds .vimrc
-cat "$(pwd)/.vimrc" > ~/.vimrc
+# Adds .vimrc - overwrites any existing vimrc
+cat "$(pwd)/vimrc" > ~/.vimrc
